67341973de94e0c139fb8d5375e7dfb895178e27
Your Name <ariunzayaccn@gmail.com>
2022-10-05 15:04:27 +0200

    first commit

---
+++ zaybol_rps/src/games/Fist.java
@@ -0,0 +1,15 @@
+package games;
+import java.util.Random;
+
+public class Fist {
+    public static final int ROCK = 0;
+    public static final int PAPER = 1;
+    public static final int SCISSORS = 2;
+
+    // en metod när den anrapas
+    public int play() {
+        Random playRandom = new Random();
+        int result = playRandom.nextInt(2)+1;
+        return result;
+    }
+}
